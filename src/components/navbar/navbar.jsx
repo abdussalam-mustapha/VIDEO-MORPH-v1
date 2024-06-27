17724a6ef2a0687382afab3360f57da7a9a75b8d
Abdussalam Mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2024-06-27 07:05:00 -0700

    fixing display issues

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,35 +1,35 @@
 "use client";
 
-import React, { useState } from 'react';
-import { DownOutlined, MenuOutlined } from '@ant-design/icons';
-import { Dropdown, message, Space } from 'antd';
+import React, { useState } from "react";
+import { DownOutlined, MenuOutlined } from "@ant-design/icons";
+import { Dropdown, message, Space } from "antd";
 
 const onClick = ({ key }) => {
   message.info(`Click on item ${key}`);
 };
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false); 
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen); 
+    setMenuOpen(!menuOpen);
   };
 
   const items = [
     {
-      label: 'Tools',
-      key: '',
-      dropdownItems: [{ key: 'video', label: 'video transcoder' }],
+      label: "Tools",
+      key: "",
+      dropdownItems: [{ key: "video", label: "video transcoder" }],
     },
     {
-      label: 'Documentation',
-      key: '',
-      dropdownItems: [{ key: 'images', label: 'coming soon' }],
+      label: "Documentation",
+      key: "",
+      dropdownItems: [{ key: "images", label: "coming soon" }],
     },
     {
-      label: 'Help',
-      key: '',
-      dropdownItems: [{ key: 'pricing', label: 'coming soon' }],
+      label: "Help",
+      key: "",
+      dropdownItems: [{ key: "pricing", label: "coming soon" }],
     },
     // {
     //   label: 'Resources',
@@ -42,31 +42,44 @@ const Navbar = () => {
   ];
 
   return (
-    <div className='flex justify-between items-center pt-6 text-[#fff] z-[1000] relative'>
-      <section className='flex space-x-4'>
-        <p className="text-[#fff]">V</p>
-        <p className="text-[#fff]">M</p>
-      </section>
-      <div className='md:hidden'>
-        <MenuOutlined onClick={toggleMenu} className='cursor-pointer' /> 
+    <div className="flex justify-between items-center pt-6 text-[#fff] z-[1000] relative">
+      {() => {
+        <section className="flex space-x-4">
+          <p className="text-[#fff]">V</p>
+          <p className="text-[#fff]">M</p>
+        </section>
+      }}
+      <div className="md:hidden">
+        <MenuOutlined onClick={toggleMenu} className="cursor-pointer" />
       </div>
-      <div className={`md:flex ${menuOpen ? 'block' : 'hidden'} absolute md:relative right-0 top-16 md:top-auto bg-white md:bg-transparent w-full md:w-auto z-50`}>
-        <ul className='flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-8 p-4 md:p-0'>
+      <div
+        className={`md:flex ${
+          menuOpen ? "block" : "hidden"
+        } absolute md:relative right-0 top-16 md:top-auto bg-white md:bg-transparent w-full md:w-auto z-50`}
+      >
+        <ul className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-8 p-4 md:p-0">
           {items.map((item, index) => (
             <li key={index}>
               <Dropdown
                 overlay={
-                  <ul className='bg-white p-2 shadow-md'>
+                  <ul className="bg-white p-2 shadow-md">
                     {item.dropdownItems.map((dropdownItem, dropdownIndex) => (
-                      <li key={dropdownIndex} className='p-2'>
-                        <a href={`/${dropdownItem.key}`} onClick={(e) => onClick(e, dropdownItem)}>
+                      <li key={dropdownIndex} className="p-2">
+                        <a
+                          href={`/${dropdownItem.key}`}
+                          onClick={(e) => onClick(e, dropdownItem)}
+                        >
                           {dropdownItem.label}
                         </a>
                       </li>
                     ))}
                   </ul>
-                }>
-                <a onClick={(e) => e.preventDefault()} className='flex items-center'>
+                }
+              >
+                <a
+                  onClick={(e) => e.preventDefault()}
+                  className="flex items-center"
+                >
                   <Space>
                     <span>{item.label}</span>
                     <DownOutlined />
@@ -75,20 +88,20 @@ const Navbar = () => {
               </Dropdown>
             </li>
           ))}
-          <li className='md:hidden'>
-            <a href='#'>Login</a>
+          <li className="md:hidden">
+            <a href="#">Login</a>
           </li>
-          <li className='md:hidden'>
-            <a href='#'>Signup</a>
+          <li className="md:hidden">
+            <a href="#">Signup</a>
           </li>
         </ul>
       </div>
-      <section className='hidden md:flex space-x-4'>
+      <section className="hidden md:flex space-x-4">
         <div>
-          <a href='#'>Login</a>
+          <a href="#">Login</a>
         </div>
         <div>
-          <a href='#'>Signup</a>
+          <a href="#">Signup</a>
         </div>
       </section>
     </div>
