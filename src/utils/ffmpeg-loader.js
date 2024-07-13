b3e02ba490d64ae3dd88eb92d26987e84b2ee11b
Abdussalam Mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2024-07-13 14:22:21 -0700

    fix git conflicts

--- src/utils/ffmpeg-loader.js
+++ src/utils/ffmpeg-loader.js
@@ -3,11 +3,10 @@ import { toBlobURL } from '@ffmpeg/util';
 
 export default async function loadFfmpeg() {
   const ffmpeg = new FFmpeg();
-const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.2/dist/umd'
+const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd'
   await ffmpeg.load({
     coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
     wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
   });
   return ffmpeg;
 }
-
